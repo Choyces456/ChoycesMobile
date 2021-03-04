e150634225f36a040b5774cbba219cad4cebae00
Kari Burt <kandreahburt@gmail.com>
2021-03-04 17:42:02 -0600

    Created Input Component

---
+++ components/input.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { TextInput, View, Text, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+/* This is the input component. The <Text> is just to make sure text is being passed. I still need to figure out it the value is saved in different input components
+*/
+const Input = ({ iconName, value, onChangeText, placeholder, placeholderTextColor, secureTextEntry, ...props }) => {
+    const { inputStyle, containerStyle } = styles;
+    const [text, setText] = useState('');
+    return (
+        <View style={{ ...styles.containerStyle, ...props.style }}>
+            <Ionicons name={iconName}size={24} color="white" style={styles.icon} />
+            <TextInput
+                secureTextEntry={secureTextEntry}
+                placeholder={placeholder}
+                placeholderTextColor={placeholderTextColor}
+                autoCorrect={false}
+                style={inputStyle}
+                onChangeText={text => setText(text)}
+                value={text}
+                autoCapitalize='none'>
+            </TextInput>
+            <Text>{text}</Text> 
+        </View>
+    )
+};
+const styles = StyleSheet.create({
+    containerStyle: {
+        height: 35,
+        borderRadius: 50,
+        marginTop: 15,
+        width: '80%',
+        alignSelf: 'center',
+        fontSize: 18,
+        flexDirection:'row',
+        alignItems:'center',
+    },
+    icon: {
+        paddingLeft:20,
+        paddingRight: 5,
+    },
+    inputStyle: {
+        alignItems:'center',
+        color: 'white',
+        fontSize: 20,
+        paddingTop: 5,
+    }
+});
+
+export default Input;
+
--- screens/LoginPage.js
+++ screens/LoginPage.js
@@ -1,76 +1,67 @@
 import React from 'react';
-import {StyleSheet, View, Image} from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
+import { StyleSheet, View, Image } from 'react-native';
+
+
 
 /* 
 I am currently using the custom component import, but I will change it to a standalone since it'll be a submission button. Also, I think I will turn these inputs into pure components tomorrow 
+
+Update 1: Created an input component, but not sure about passing down values, will look into "props." Also needs a true submit button. secureTextEntry deals with obscuring password input. 
 */
 
 import RoundBtn from '../components/button';
+import Input from '../components/input';
 
 
 
-function LoginPage({navigation}){
-    return(
-    <View style={styles.screen}>
+function LoginPage({ navigation }) {
+    return (
+        <View style={styles.screen}>
 
-    <Image
-    source={require('../assets/images/choyceslogo.jpeg')}
-    style={styles.image}/> 
+            <Image
+                source={require('../assets/images/choyceslogo.jpeg')}
+                style={styles.image} />
 
-    <TextInput
-    style={styles.username}
-    placeholder='Username'
-    placeholderTextColor='white'
-    //onChangeText={text => onChangeText(text)}
-    />
+            <Input
+                style={styles.username}
+                placeholder='Username'
+                placeholderTextColor='white'
+                iconName='person-circle-outline'
+                />        
 
-    <TextInput
-    style={styles.password}
-    placeholder='Password'
-    placeholderTextColor='white'
-    //onChangeText={text => onChangeText(text)}
-    />
+            <Input
+                style={styles.password}
+                placeholder='Password'
+                placeholderTextColor='white'
+                iconName='lock-open-outline'
+               //secureTextEntry 
+            />
+           
+            <RoundBtn buttontext="Login"
+                style={styles.loginBtn}
+                screenName='Welcome'
+            />
 
-    <RoundBtn buttontext="Login"
-        style={styles.loginBtn}
-    />
-
-    </View>
+        </View>
 
     )
+
 };
 
-const styles=StyleSheet.create({
+const styles = StyleSheet.create({
     screen: {
-        flex:1,
+        flex: 1,
     },
-    username:{
-        backgroundColor: '#ffa500',  
-        height: 35,
-        borderRadius: 50,
-        marginTop: 15,
-        width: '80%',
-        textAlign: 'center',
-        alignSelf: 'center',
-        alignItems: "center",
-        fontSize: 18,
+    username: {
+        backgroundColor: '#ffa500',
     },
-    password:{
-        backgroundColor: '#d6204e',  
-        height: 35,
-        borderRadius: 50,
-        marginTop: 15,
-        width: '80%',
-        textAlign: 'center',
-        alignSelf: 'center',
-        alignItems: "center",
-        fontSize: 18,
 
+    password: {
+        backgroundColor: '#d6204e',
     },
     loginBtn: {
         backgroundColor: '#23b5ce',
-        marginTop:15,
+        marginTop: 15,
     },
     image: {
         width: '80%',
