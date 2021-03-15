f3696caa70d88b54b8caa3b8a1967eea4becba89
Kody <kodystumpf@gmail.com>
2021-03-15 13:15:43 -0700

    Picker/droplist fix

--- screens/GetStarted.js
+++ screens/GetStarted.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 
 import Logo from '../components/logo';
-import DropList from '../components/droplist';
+import { Picker } from 'react-native-picker-dropdown'
+import { Ionicons } from '@expo/vector-icons';
 
 
 function GetStarted({navigation}){
+    const [loginType, setLoginType] = useState("Who's using this app?");
     return(
         <View style={styles.screen}>
         <Logo/>
-        <DropList>Are you..</DropList>
+        <View style={styles.container}>
+                <Ionicons name="person-outline" size={24} color="white" style={styles.icon} />
+        <Picker
+        style={styles.picker}
+        selectedValue={loginType}
+        onValueChange={(itemValue, itemIndex) =>
+        setLoginType(itemValue)}
+        mode="dropdown"
+        textStyle={styles.pickerText}
+        >
+
+                    <Picker.Item label="Educator" value="educator" key="1"/>
+                    <Picker.Item label="Employee" value="employee" key="2"/>
+                    <Picker.Item label="Parent" value="parent" key="3"/>
+                    <Picker.Item label="Student" value="student"  key="4"/>
+
+        </Picker>
+        </View>
 
 
 
@@ -30,9 +49,46 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
     },
+    container: {
+        flex: 0.4,
+        flexDirection: 'row',
+        width: '95%',
+        minHeight: 40,
+        maxHeight: 40,
+        borderRadius: 20,
+        backgroundColor: '#ffa500',
+        alignSelf: 'center',
+        alignItems: 'baseline'
+
+
+    },
+    icon: {
+        paddingLeft: 10,
+        paddingTop: 5,
+    },
+    picker: {
+        flex: 1,
+        alignSelf:'flex-end',
+        height: 20,
+        
+
+    },
+    pickerText: {
+        flex: 1,
+        color: '#FFFFFF',
+        fontSize: 22,
+        textAlign: 'center'
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+    },
     bottomLink: {
-        marginTop: '75%',
+        flex: 1,
+        justifyContent: 'flex-end',
         alignItems: 'center',
+        marginBottom: 20,
     }
 });
 
